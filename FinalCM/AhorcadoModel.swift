3c3a0c5a7425b3a365287ad85d8f64ac75505ff8
Antonio <josantorres93@gmail.com>
2015-11-08 21:05:34 -0600

    Added buttons for every letter in the abecedary

---
+++ FinalCM/AhorcadoModel.swift
@@ -0,0 +1,17 @@
+//
+//  AhorcadoModel.swift
+//  FinalCM
+//
+//  Created by Antonio Santiago on 11/8/15.
+//  Copyright © 2015 Abner Castro Aguilar. All rights reserved.
+//
+//  DESCRIBES THE LOGIC OF THE AHORCADO OPTION.
+//  FUNCTIONALITY OF THIS CLASS IS: 
+//  1. Determine the letters in the word searched
+//  2. Develop the functions to display images
+
+import Foundation
+
+class AhorcadoModel: NSObject {
+
+}
--- FinalCM/AhorcadoViewController.swift
+++ FinalCM/AhorcadoViewController.swift
@@ -11,27 +11,98 @@ import UIKit
 class AhorcadoViewController: UIViewController {
     
     // MARK: ATTRIBUTES
+    var category = ""
+    var word = ""
+
+    let ahorcadoXML = AhorcadoXMLParser()
+    
+    // MARK: OUTLETS
     
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     
-    let ahorcadoXML = AhorcadoXMLParser()
-    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+
     // MARK: METHODS
 
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        self.navigationItem.title = "Ahorcado"
-        
+    func fetchWords() {
         // Getting text for wordLabel and categoryLabel from Server
         ahorcadoXML.parseXML()
-        categoryLabel.text = String(ahorcadoXML.elements.objectForKey("CATEGORIA")!)
-        wordLabel.text = String(ahorcadoXML.elements.objectForKey("PALABRA")!)
-        
-        /*
-        categoryLabel.text = ahorcadoXML.elements.objectForKey("CATEGORIA") as? String      // nil
-        wordLabel.text = ahorcadoXML.elements.objectForKey("PALABRA") as? String            // nil
-        */
+        category = String(ahorcadoXML.elements.objectForKey("CATEGORIA")!)
+        word = String(ahorcadoXML.elements.objectForKey("PALABRA")!)
+        categoryLabel.text = category
+        wordLabel.text = word
+    }
+    
+    func displayWord() {
+        for character in word.characters {
+            var character = UIImage(named: "\(character).png")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Ahorcado"
+        fetchWords()
+    }
+    
+    // MARK: ACTIONS
+    
+    @IBAction func aPressed(sender: UIButton) {
+        aButton.enabled = false
+    }
+    @IBAction func bPressed(sender: UIButton) {
+        bButton.enabled = false
+    }
+    @IBAction func cPressed(sender: UIButton) {
+    }
+    @IBAction func dPressed(sender: UIButton) {
+    }
+    @IBAction func ePressed(sender: UIButton) {
+    }
+    @IBAction func fPressed(sender: UIButton) {
+    }
+    @IBAction func gPressed(sender: UIButton) {
+    }
+    @IBAction func hPressed(sender: UIButton) {
+    }
+    @IBAction func iPressed(sender: UIButton) {
+    }
+    @IBAction func jPressed(sender: UIButton) {
+    }
+    @IBAction func kPressed(sender: UIButton) {
+    }
+    @IBAction func lPressed(sender: UIButton) {
+    }
+    @IBAction func mPressed(sender: UIButton) {
+    }
+    @IBAction func nPressed(sender: UIButton) {
+    }
+    @IBAction func niPressed(sender: UIButton) {
+    }
+    @IBAction func oPressed(sender: UIButton) {
+    }
+    @IBAction func pPressed(sender: UIButton) {
+    }
+    @IBAction func qPressed(sender: UIButton) {
+    }
+    @IBAction func rPressed(sender: UIButton) {
+    }
+    @IBAction func sPressed(sender: UIButton) {
+    }
+    @IBAction func tPressed(sender: UIButton) {
+    }
+    @IBAction func uPressed(sender: UIButton) {
+    }
+    @IBAction func vPressed(sender: UIButton) {
+    }
+    @IBAction func wPressed(sender: UIButton) {
+    }
+    @IBAction func xPressed(sender: UIButton) {
+    }
+    @IBAction func yPressed(sender: UIButton) {
+    }
+    @IBAction func zPressed(sender: UIButton) {
     }
 }
